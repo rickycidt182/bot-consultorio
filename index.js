82e90de568230b67593a6c13f495069e72f9863a
rickycidt182 <rickycidt@gmail.com>
2026-04-09 09:37:07 -0600

    Update index.js

--- index.js
+++ index.js
@@ -45,5 +45,5 @@ app.post("/whatsapp", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(\`Servidor corriendo en puerto ${PORT}\`);
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
