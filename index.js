bfcdb092a28af303c55638e299d0e939650a3cd9
rickycidt182 <rickycidt@gmail.com>
2026-04-09 11:15:59 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,29 +6,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
+  console.log("GET /");
   res.status(200).send("Servidor activo ✅");
 });
 
 app.post("/whatsapp", (req, res) => {
-  console.log("📩 Webhook recibido:", JSON.stringify(req.body));
+  console.log("POST /whatsapp");
+  console.log("Body:", JSON.stringify(req.body));
 
-  res.set("Content-Type", "text/xml");
-  return res.status(200).send(`
-    <Response>
-      <Message>Hola 😊 Ya quedó conectado el bot.</Message>
-    </Response>
-  `);
+  res.type("text/xml");
+  res.status(200).send(
+    '<?xml version="1.0" encoding="UTF-8"?><Response><Message>Hola 😊 Ya quedó conectado el bot.</Message></Response>'
+  );
 });
 
 app.post("/whatsapp/", (req, res) => {
-  console.log("📩 Webhook recibido slash:", JSON.stringify(req.body));
+  console.log("POST /whatsapp/");
+  console.log("Body:", JSON.stringify(req.body));
 
-  res.set("Content-Type", "text/xml");
-  return res.status(200).send(`
-    <Response>
-      <Message>Hola 😊 Ya quedó conectado el bot.</Message>
-    </Response>
-  `);
+  res.type("text/xml");
+  res.status(200).send(
+    '<?xml version="1.0" encoding="UTF-8"?><Response><Message>Hola 😊 Ya quedó conectado el bot.</Message></Response>'
+  );
 });
 
 const PORT = process.env.PORT || 3000;
